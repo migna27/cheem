6495bc5c24cbb415b2b1b0d334796199786e56b6
Miguel <gosvaldo372@gmail.com>
2025-11-12 08:46:24 -0700

    entities, persistence

---
+++ entities/winner.py
@@ -0,0 +1,12 @@
+from persistence.db import get_db_connection
+class Winner:
+    def __init__(self,id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+
+    def save(self):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        query = "INSERT INTO winners (name, email) VALUES (%s, %s)"
+        cursor.execute(query, (self.name, self.email))
