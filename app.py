489d8edf0c8133aec2aee4cb2802883f98c77876
Miguel <gosvaldo372@gmail.com>
2025-11-18 10:06:46 -0700

    Cambios de la clase pasada

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from entities.winner import Winner
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/winner', methods=['POST'])
+def save_winner():
+    data = request.get_json()
+
+    winner = Winner(id=0 ,name=data['name'], email=data['email'])
+
+    winner.save()
+    if winner.id != 0:
+        return jsonify ({"success": True, "id": winner.id}), 201
+    else:
+        return jsonify ({"success": False}), 500
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
--- entities/winner.py
+++ entities/winner.py
@@ -6,7 +6,18 @@ class Winner:
         self.email = email
 
     def save(self):
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        query = "INSERT INTO winners (name, email) VALUES (%s, %s)"
-        cursor.execute(query, (self.name, self.email))
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            query = "INSERT INTO winners (name, email) VALUES (%s, %s)"
+            cursor.execute(query, (self.name, self.email))
+            connection.commit()
+
+            self.id = cursor.lastrowid
+            return self.id
+        except Exception as ex:
+            print("Error al guardar el registro:", ex)
+            return 0
+        finally:
+            cursor.close()
+            connection.close()
